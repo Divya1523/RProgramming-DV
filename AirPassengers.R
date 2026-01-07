78bf34ab70a664f5c388b8547482dde094458e11
Divya1523 <147472516+Divya1523@users.noreply.github.com>
2026-01-07 11:56:09 +0530

    Add files via upload

---
+++ AirPassengers.R
@@ -0,0 +1,68 @@
+AirPassengers
+
+class(AirPassengers)
+View(AirPassengers)
+
+ap_df <- data.frame(
+  year = time(AirPassengers),
+  passengers = as.numeric(AirPassengers)
+)
+
+ap_df_months <- data.frame(
+  year = floor(time(AirPassengers)),
+  month = cycle(time(AirPassengers)),
+  passengers = as.numeric(AirPassengers)
+)
+
+ap_df_months
+
+plot(AirPassengers)
+
+plot(AirPassengers,
+     main = "Air Passengers Trend Analysis",
+     xlab = "Months",
+     ylab = "No. of Passengers",
+     col = "blue"
+)
+
+plot(AirPassengers,
+     type = 'l', #for line
+     lwd = 2, #line width
+     main = "Air Passengers Trend Analysis",
+     xlab = "Months",
+     ylab = "No. of Passengers",
+     col = "blue"
+)
+
+plot(AirPassengers,
+     type = 'o', #for point
+     lwd = 0.9, #line width
+     main = "Air Passengers Trend Analysis",
+     xlab = "Months",
+     ylab = "No. of Passengers",
+     col = "blue"
+)
+points(AirPassengers,
+       type = 'o',
+       pch = 17,
+       cex = 1.2,
+       col = "red")
+grid()
+
+# ggplot line plot
+library(ggplot2)
+
+ggplot(ap_df, 
+       aes(x = year,
+           y = passengers)) +
+  geom_line(color = "black", linewidth = 0.5) +
+  labs(
+    title = "Air Passengers Trend Analysis",
+    caption = "From  AirPassengers dataset",
+    x = "Month",
+    y = "Number of Passengers",
+    subtitle = "From 1949 to 1960"
+  ) +
+  geom_point(color = "blue")+
+  geom_smooth(se=FALSE, color = 'red')+
+  theme_light()
